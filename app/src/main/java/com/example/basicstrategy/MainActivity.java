9dafd65be38d25ceb35daca47d8c8eccd59d214b
ktran151 <ktran151@users.noreply.github.com>
2019-05-08 18:42:41 -0400

    initial commit

---
+++ app/src/main/java/com/example/basicstrategy/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.basicstrategy;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.widget.TextView;
+
+public class MainActivity extends WearableActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text);
+        System.out.println("hey again");
+
+        // Enables Always-on
+        setAmbientEnabled();
+    }
+}
